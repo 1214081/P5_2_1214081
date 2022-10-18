e937f28b2e5370db436230a11d84ded194627543
Ibrohim Mubarok <114393418+1214081@users.noreply.github.com>
2022-10-18 20:55:31 +0700

    Add files via upload

---
+++ P5_2_1214081/P5_2_1214081/ParentForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_2_1214081
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildeForm newChilde  = new ChildeForm();
+            newChilde.MdiParent = this;
+            newChilde.Show();
+        }
+    }
+}
